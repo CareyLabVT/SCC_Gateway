5db0f80a00eb98efe1fd2fcd72ffe72e5525e7a3
Vahid Daneshmand <v.daneshmand@gmail.com>
2021-02-15 11:45:03 -0500

    Add Progress Details to the Log

--- git-gc.sh
+++ git-gc.sh
@@ -6,18 +6,26 @@
 # Usage: Add the following line to crontab to run it on the first day of each month at 12:00 AM
 # 00 00 01 * * git-gc.sh
 
+location=fcre-catwalk
 logfile=/data/$HOSTNAME-logs/git-gc.log
-datadir=/data/$HOSTNAME-data
+datadir=/data/$location-data
 logdir=/data/$HOSTNAME-logs
+#git_exec=/home/scc/applications/git-retry.sh
 
-timestamp=$(date +"%D %T %Z %z")
+timestamp=$(date +"%D %T %Z")
 
-echo -e "\n\n#################################################################\n" 2>&1 | tee -a $logfile
-echo -e "############## $HOSTNAME - $timestamp ##############" 2>&1 | tee -a $logfile
-echo -e "\n#################################################################\n\n" 2>&1 | tee -a $logfile
+echo -e "############################ $HOSTNAME - $timestamp ############################" 2>&1 | tee -a $logfile
+
+echo "Disk Usage before Git Garbage Collection:"
+df -h | grep /data 2>&1 | tee -a $logfile
 
 cd $datadir
+echo "Working on: $(pwd)"
 git gc --prune 2>&1 | tee -a $logfile
 
 cd $logdir
+echo "Working on: $(pwd)"
 git gc --prune 2>&1 | tee -a $logfile
+
+echo "Disk Usage after Git Garbage Collection:"
+df -h | grep /data 2>&1 | tee -a $logfile
